64152c81b38566f0cd21e3fc6352d8d7998c70a6
Philipp Arndt <philipp.arndt@vector.com>
2026-09-10 19:34:08 +0200

    Abydos 0.20.4

--- Casks/abydos.rb
+++ Casks/abydos.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "abydos" do
-  version "0.20.3"
-  sha256 "7dc2d07e39162f7a9658f978423f656e936e9f016133cb969e43711e4736bda9"
+  version "0.20.4"
+  sha256 "b0df3e4731f4b0b18e7b34c47565cdd56c9c66301d1b22709214f8f2d3271e35"
 
   url "https://github.com/philipparndt/abydos/releases/download/v#{version}/Abydos-#{version}.dmg"
   name "Abydos"
